823afa6af1fc6d1e2377f27a27e8ed469a6320ea
Sachin Kiran Shah <sachinksha@gmail.com>
2026-05-14 19:23:43 +0530

    cleanup of project using github copilot

--- src/bitebook/context.jsx
+++
@@ -1,6 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const AppCtx = createContext(null);
-export const ToastCtx = createContext(null);
-export const useApp = () => useContext(AppCtx);
-export const useToastCtx = () => useContext(ToastCtx);
--- src/bitebook/globals.js
+++
@@ -1,5 +0,0 @@
-import * as XLSX from "xlsx";
-import Chart from "chart.js/auto";
-
-window.XLSX = XLSX;
-window.Chart = Chart;
